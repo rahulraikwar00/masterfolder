0c3e858b94abbcec6f4ce5cd17db6d12958aad82
rahul <rahu@fadfsa.com>
2020-12-15 02:20:21 +0530

    arangement

--- cpp2/testfile.py
+++
@@ -1,8 +0,0 @@
-import collections
-ls = list(input().split())
-# a= collections.Counter(ls)
-# ls2=[]
-# for i in list(a):
-# 	ls2.append(ls.count(i))
-# print(len(list(a)),*ls2)
-print(len(set()), )
